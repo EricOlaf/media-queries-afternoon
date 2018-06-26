128ac47aedc56973ecbf9fa7ab6da37b027bfe4d
Eric Olaveson <Eric@Erics-MacBook-Pro.local>
2018-06-26 17:13:48 -0500

    Done

---
+++ index.js
@@ -0,0 +1,9 @@
+function openmenu() {
+  var menu = document.querySelector(".nav__dropdown");
+  if (!menu.classList.contains("nav__dropdown--show")) {
+    menu.classList.add("nav__dropdown--show");
+    menu.classList.remove("small");
+  } else {
+    menu.classList.remove("nav__dropdown--show");
+  }
+}
